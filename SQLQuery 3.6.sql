11284e72ec4fbe921663aaaa566b096a2e918b51
Megan Moore <meganmoore.dev@gmail.com>
2023-08-11 09:47:50 -0400

    Reviewed Exercise Answers

--- SQLQuery 3.6.sql
+++ SQLQuery 3.6.sql
@@ -1,8 +1,6 @@
 --ch3.6 Write a Select statement that returns one column from the Vendors table called Full Name. Modify.
 SELECT VendorContactLName + ', ' + VendorContactFName as 'Full Name'
 From Vendors
-WHERE VendorContactLName not in 
-        SELECT vendorcontactlname
-        from Vendors
-        WHERE like '[a - e]'
+WHERE VendorContactLName LIKE '[a-c,e]%'
 ORDER BY VendorContactLName, VendorContactFName;
+
--- SQLQuery 3.7.sql
+++ SQLQuery 3.7.sql
@@ -1,7 +1,7 @@
 --ch 3.7 Write a select statement that determine the paymentdate column...
-SELECT PaymentDate
+SELECT *
 FROM Invoices
-WHERE InvoiceTotal [not] null
-        SELECT *
-        FROM NullSample
-        WHERE invoiceTotal
+WHERE ((InvoiceTotal - PaymentTotal - CreditTotal <= 0) AND
+                PaymentDate IS NULL) OR
+      ((InvoiceTotal - PaymentTotal - CreditTotal > 0) AND
+                PaymentDate IS NOT NULL);
